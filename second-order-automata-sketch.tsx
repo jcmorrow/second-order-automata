1e9b60cdce46afc6b03c0639383fb936811517fb
Josh Morrow <josh@jcmorrow.com>
2021-06-12 17:52:47 -0700

    Remove built files

--- second-order-automata-sketch.tsx
+++ second-order-automata-sketch.tsx
@@ -1,17 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Sketch from "react-p5";
 import p5Types from "p5";
 
-declare global {
-  interface Window {
-    rule: number;
-    secondOrder: boolean;
-  }
-}
-
 import { randomBinaryString, nextFromPredecessors, clamp } from "./utils";
 
-const CELL_WIDTH = 128;
+const CELL_WIDTH = 256;
 const PIXEL_SIZE = 4;
 export const c = clamp(CELL_WIDTH);
 
@@ -19,13 +12,14 @@ export const c = clamp(CELL_WIDTH);
 const twoPrevRow: Array<number> = randomBinaryString(CELL_WIDTH);
 const prevRow: Array<number> = randomBinaryString(CELL_WIDTH);
 
-const render = (cells: Array<Array<number>>, p: p5Types) => {
-  cells.forEach((row, y) => {
+const render = (rows: Array<Array<number>>, p: p5Types) => {
+  rows.forEach((row, y) => {
     row.forEach((cell, x) => {
       if (cell) {
-        // square(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE);
-        p.square(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE);
-        p.fill(200, 200, 200);
+        if (Math.random() > 0.0) {
+          p.square(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE);
+          // p.fill(200, 200, 200);
+        }
       }
     });
   });
@@ -36,12 +30,13 @@ const cells: Array<Array<number>> = [twoPrevRow, prevRow];
 const iterate = (
   lastGen: Array<number>,
   twoGensAgo: Array<number>,
-  useSecondOrder: boolean
+  useSecondOrder: boolean,
+  rule: number
 ) =>
   lastGen.map((_x, i) => {
     const firstOrder = nextFromPredecessors(
       [lastGen[c(i - 1)], lastGen[c(i)], lastGen[c(i + 1)]],
-      window.rule
+      rule
     );
     const secondOrder = twoGensAgo[i];
     if (useSecondOrder) {
@@ -60,6 +55,8 @@ const iterate = (
   });
 
 const SecondOrderAutomataSketch: React.FC<Record<string, never>> = () => {
+  const [state] = useState({ secondOrder: true, rule: 30 });
+
   const setup = (p: p5Types, canvasParentRef: Element) => {
     p.createCanvas(500, 500).parent(canvasParentRef);
   };
@@ -67,15 +64,17 @@ const SecondOrderAutomataSketch: React.FC<Record<string, never>> = () => {
   const draw = (p: p5Types) => {
     p.clear();
 
-    const newCells = iterate(
-      cells[cells.length - 1],
-      cells[cells.length - 2],
-      window.secondOrder
-    );
+    // const newCells = iterate(
+    //   cells[cells.length - 1],
+    //   cells[cells.length - 2],
+    //   state.secondOrder,
+    //   state.rule
+    // );
     if (cells.length > CELL_WIDTH) {
-      cells.shift();
+      // cells.shift();
+      p.noLoop();
     }
-    cells.push(newCells);
+    cells.push(randomBinaryString(CELL_WIDTH));
 
     render(cells, p);
   };
